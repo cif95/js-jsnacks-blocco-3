ec2a401cb59c9387f9e92cdf2af75251185bc343
cif95 <vivianacifarelli9@gmail.com>
2022-03-03 13:15:09 +0100

    added functions comments

--- jsnack-4/script.js
+++ jsnack-4/script.js
@@ -1,8 +1,10 @@
 // creo una funzione che controlla se la somma di due numeri x e y sia pari
 
-// funzione che verifica se un numero è pari
-// se è pari, restituisce true
-// altrimenti restituisce false
+/**
+ * function the checks is a number is even
+ * @param {*} number number to be checked
+ * @returns true ( if number is even) ; otherwise false.
+ */
 function isEven (number) {
 	if ( number % 2 === 0 ) {
 		return true;
@@ -10,6 +12,12 @@ function isEven (number) {
 	return false;
 }
 
+/**
+ * function that checks if the sum between two numbers is even
+ * @param {*} num1 first number to be added
+ * @param {*} num2 second number to be added
+ * @returns true if the sum of the two numbers is even ; otherwise false.
+ */
 function isSumEven ( num1, num2) {
 	return isEven(num1 + num2);
 }
